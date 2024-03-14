9f4152330d49941d9cf772fa25802337854d5cbb
usernameIV <researches.dev.pro@gmail.com>
2024-03-14 14:17:04 -0300

    First commit

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -6,7 +6,7 @@ public class Exercicio1 {
 
             int[][] matriz = new int[2][4];
             Scanner scanner = new Scanner(System.in);
-           
+
             for (int i = 0; i < 2; i++) {
                 for (int j = 0; j < 4; j++) {
 
